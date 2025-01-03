ec094101feac972c0a125b3ce34f9b0358c41340
unknown <suporte@TI-02.dnordibe.com.br>
2025-01-03 12:50:54 -0300

    chore: metodo map adicionado para renderização das divs

--- app/components/Quadrado.tsx
+++ app/components/Quadrado.tsx
@@ -1,31 +1,15 @@
-// const caracters = ['%', 'CE', 'C','⌫', '¹/ₓ', '𝑥²', '²√𝑥','÷', '7', '8', '9','X', '4', '5', '6', '-', '1', '2', '3', '+/-', '0', ',', '=' ]
+const caracters = ['%', 'CE', 'C','⌫', '¹/ₓ', '𝑥²', '²√𝑥','÷', '7', '8', '9','X', '4', '5', '6', '-', '1', '2', '3', '+','+/-', '0', ',']
 
 export default function Quadrado(){
     return (
         <div className="pai">
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">%</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">CE</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">C</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">⌫</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">¹/ₓ</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">𝑥²</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">²√𝑥</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">÷</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">7</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">8</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">9</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">X</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">4</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">5</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">6</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">-</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">1</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">2</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">3</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">+</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">+/-</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">0</div>
-            <div className="filho bg-zinc-600 hover:bg-zinc-700">,</div>
+            {caracters.map((item,index) => {
+               return (
+                <div key = {index} className="filho bg-zinc-600 hover:bg-zinc-700">{item}</div>
+               ) 
+            })}
+            
+           
             <div className="filho bg-pink-300 hover:bg-opacity-90 text-black">=</div>
         </div>
     )
